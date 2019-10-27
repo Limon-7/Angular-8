ecef1255167fa41eab92f5e672cc01da7403fa40
limon-7 <liibd7@gmail.com>
2019-10-27 23:36:00 +0600

    Data Binding& Basic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fastgameon-app';
+  title = 'My Sweet Angular 8 app';
+  name = 'limon';
 }
