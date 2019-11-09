f1026e17cd7febf68f8783f8dd4f4d7d7b103496
Limon-7 <liibd7@gmail.com>
2019-11-09 20:25:16 +0600

    Form control

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -7,6 +7,34 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent {
+  contactMethod: any = [
+    {
+      id: 1,
+      name: 'Email'
+    },
+    {
+      id: 2,
+      name: 'Facebook'
+    },
+    {
+      id: 3,
+      name: 'Twitter'
+    },
+  ];
+  gender: any = [
+    {
+      id: 1,
+      name: 'Male'
+    },
+    {
+      id: 2,
+      name: 'Female'
+    },
+    {
+      id: 3,
+      name: 'Other'
+    },
+  ];
   log(x) { console.log(x); }
   onSubmit(f) {
     console.log(f);
